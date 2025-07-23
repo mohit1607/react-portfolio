456b1e905b616eeb3b4f09ef1650ac5110258c22
mohit1607 <57750800+mohit1607@users.noreply.github.com>
2025-07-23 14:40:04 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
 export const linkedinURL = 'https://www.linkedin.com/in/mohit-ashliya/'
 export const xURL = 'https://x.com/pitoorpitoor'
 export const githubURL = 'https://github.com/mohit1607'
+// nothing is constant as I have to make this change so that vercel can deploye my app
